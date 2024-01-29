c0894782183f8cb285366ede254221f20a2a516d
chnmk <a2molchanov@ya.ru>
2024-01-29 15:11:38 +0300

    feat: change printed JSON type to DescriptionList

--- src/jsonGenerator/jsonGenerator.go
+++ src/jsonGenerator/jsonGenerator.go
@@ -6,31 +6,49 @@ import (
 	"os"
 )
 
-type MyJSON struct {
-	ArtistName string `json:"artist"`
-	SongName   string `json:"name"`
-	FileName   string `json:"file"`
+type ContentsType struct {
+	Category string   `json:"Category"`
+	Entries  []string `json:"Entries,omitempty"`
+}
+
+type DescriptionList struct {
+	Name        string         `json:"Name"`
+	Description string         `json:"Description,omitempty"`
+	Tags        []string       `json:"Tags,omitempty"`
+	Contents    []ContentsType `json:"Contents,omitempty"`
 }
 
 func GenerateJSON() string {
 
-	fileNames, err := os.ReadDir("./temp")
+	// Read files from ./temp
+	files, err := os.ReadDir("./temp")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var fileName []string
-	for _, file := range fileNames {
-		fileName = append(fileName, file.Name())
+	// Declare required variables
+	var filesOutput []string
+	var newContent ContentsType
+
+	// Get variables from files
+	for _, file := range files {
+		filesOutput = append(filesOutput, file.Name())
+		newContent = ContentsType{
+			Category: "MyCat",
+			Entries:  filesOutput,
+		}
 	}
 
-	myResult := &MyJSON{
-		ArtistName: "My Favourite Artist",
-		SongName:   "Best Song Ever",
-		FileName:   fileName[1],
+	// Build a DescriptionList
+	myResult := &DescriptionList{
+		Name:        "MyName",
+		Description: "MyDesc",
+		Tags:        []string{"Tag1", "Tag2"},
+
+		Contents: []ContentsType{newContent},
 	}
 
-	myResultB, _ := json.Marshal(myResult)
-
-	return string(myResultB)
+	// Return result
+	myResultMarshal, _ := json.Marshal(myResult)
+	return string(myResultMarshal)
 }
